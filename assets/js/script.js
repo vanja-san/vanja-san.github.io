947e6bfb0f3a2b11714bcb5c379dacd5d3d77c2f
vanja-san <vanjavs@mail.ru>
2024-10-05 12:10:14 +0700

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,20 @@
-const themeToggleBtn = document.querySelector(".theme-toggle-btn");
-const body = document.body;
+const themeToggle = document.getElementById('theme-toggle');
 
-let isDarkTheme = false;
+// Get the current theme state from localStorage
+const currentTheme = localStorage.getItem('theme');
 
-themeToggleBtn.addEventListener("click", () => {
-  if (isDarkTheme) {
-    body.classList.remove("dark-theme");
-    body.classList.add("light-theme");
-    themeToggleBtn.querySelector(".ti-moon").style.display = "inline-block";
-    themeToggleBtn.querySelector(".ti-sun").style.display = "none";
-    localStorage.setItem("theme", "light");
-  } else {
-    body.classList.remove("light-theme");
-    body.classList.add("dark-theme");
-    themeToggleBtn.querySelector(".ti-moon").style.display = "none";
-    themeToggleBtn.querySelector(".ti-sun").style.display = "inline-block";
-    localStorage.setItem("theme", "dark");
-  }
-  isDarkTheme = !isDarkTheme;
-});
-
-// Set initial theme based on localStorage
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add("dark-theme");
-  themeToggleBtn.querySelector(".ti-moon").style.display = "none";
-  themeToggleBtn.querySelector(".ti-sun").style.display = "inline-block";
-  isDarkTheme = true;
-} else {
-  body.classList.add("light-theme");
-  themeToggleBtn.querySelector(".ti-moon").style.display = "inline-block";
-  themeToggleBtn.querySelector(".ti-sun").style.display = "none";
+// Set the initial theme state
+if (currentTheme === 'dark-mode') {
+  document.body.classList.add('dark-mode');
 }
+
+themeToggle.addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+
+  // Update the theme state in localStorage
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark-mode');
+  } else {
+    localStorage.removeItem('theme');
+  }
+});
